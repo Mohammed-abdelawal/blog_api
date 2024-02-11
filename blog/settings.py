c48e406f1c0ccc4aa138a38d31e840e26d86ca80
mabdelawal <mohammedabdelawaldeveloper@gmail.com>
2024-02-11 17:50:49 +0200

    add installed apps

--- blog/settings.py
+++ blog/settings.py
@@ -30,6 +30,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "rest_framework.authtoken",
+    "ckeditor",
+    "core",
 ]
 
 MIDDLEWARE = [
