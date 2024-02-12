282d08fc79fa0dee5fa1501b324154310e5ce658
mabdelawal <mohammedabdelawaldeveloper@gmail.com>
2024-02-12 21:48:41 +0200

    add var to .env. format urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-                  path('api/', include('core.urls')),
-                  path('api/token/', obtain_auth_token, name='obtain-token'),
-                  path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('core.urls')),
+    path('api/token/', obtain_auth_token, name='obtain-token'),
+    path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
